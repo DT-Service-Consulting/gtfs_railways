7e4a9cf9eb8ea30f6027500dd675e23dcf712818
Marco <marco@mdigennaro.net>
2025-07-01 16:57:18 +0200

    modified setup

--- setup.py
+++ setup.py
@@ -5,9 +5,16 @@ setup(
     version='0.1.4',
     packages=find_packages(include=['gtfs_railways', 'gtfs_railways.*']),
     description='Functions to work on GTFS railways data',
-    author='Praneesh Sharma',
-    author_email='praneesh.sharma@dtsc.be',
-    url='https://github.com/Praneesh-Sharma',
+    long_description='A Python library to process and visualize GTFS (General Transit Feed Specification) data for railways.',
+    long_description_content_type='text/markdown',
+    author='Renzo Massobrio',
+    author_email='renzo.massobrio@uantwerpen.be',
+    maintainer='Marco Di Gennaro, Praneesh Sharma',
+    maintainer_email='marco.digennaro@dtsc.be, praneesh.sharma@dtsc.be',
+    url='https://github.com/DT-Service-Consulting/gtfs_railways',
+    project_urls={
+        'Homepage': 'https://github.com/DT-Service-Consulting/gtfs_railways',
+    },
     install_requires=[
         'pandas',
         'numpy',
@@ -17,8 +24,9 @@ setup(
         'geopy',
         'thefuzz',
         'ipython',
-        'ipykernel',
-        'jupyter'
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.6, <3.9',
+    classifiers=[
+        'License :: Apache 2.0',
+    ],
 )
