9026b068a03aad3b7c250ae2fd6d4fc37b93760b
Marco <marco@mdigennaro.net>
2025-07-01 18:36:27 +0200

    test py3.13

--- gtfs_railways/examples/test_import.py
+++
@@ -1,5 +0,0 @@
-import gtfs_railways
-from gtfspy import import_gtfs
-from osmread import *
-
-print('test successful!!')
--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup, find_packages
+import os
+
+# Read README.md for long_description if it exists
+this_directory = os.path.abspath(os.path.dirname(__file__))
+readme_path = os.path.join(this_directory, "README.md")
+if os.path.exists(readme_path):
+    with open(readme_path, encoding="utf-8") as f:
+        long_description = f.read()
+else:
+    long_description = "A Python library to process and visualize GTFS (General Transit Feed Specification) data for railways."
+
 
 setup(
     name='gtfs_railways',
     version='0.1.4',
     packages=find_packages(include=['gtfs_railways', 'gtfs_railways.*']),
     description='Functions to work on GTFS railways data',
-    long_description='A Python library to process and visualize GTFS (General Transit Feed Specification) data for railways.',
     long_description_content_type='text/markdown',
     author='Renzo Massobrio',
     author_email='renzo.massobrio@uantwerpen.be',
@@ -24,8 +34,8 @@ setup(
         'geopy',
         'thefuzz',
         'ipython',
+        'pytest'
     ],
-    python_requires='>=3.6, <3.9',
     classifiers=[
         'License :: Apache 2.0',
     ],
