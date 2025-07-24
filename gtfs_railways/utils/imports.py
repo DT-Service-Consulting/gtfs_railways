06b192e9003b06df127e070dd298b0fcfcbf5a32
Marco <marco@mdigennaro.net>
2025-07-24 16:16:45 +0200

    fixed imports

--- gtfs_railways/utils/imports.py
+++ gtfs_railways/utils/imports.py
@@ -1,5 +1,12 @@
 from gtfs_railways.utils.config import DATA_DIR
 
+from gtfs_railways.functions.core import load_graph
+from gtfs_railways.functions.core import load_gtfs
+from gtfs_railways.functions.core import efficiency_graph
+from gtfs_railways.functions.core import make_sp_func, simulate_fixed_node_removal_efficiency,run_removal_simulations, load_all_subgraphs
+
+from gtfs_railways.functions.v0 import P_space
+from gtfs_railways.functions.v0 import get_all_GTC
+from pprint import pprint
+
 path_to_sqlite = str( DATA_DIR / "sqlite/belgium.sqlite" )
-
-
