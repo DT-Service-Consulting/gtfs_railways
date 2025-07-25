d657f59cc54c6c7d74e109d0314c5ce9f5732f5c
Marco <marco@mdigennaro.net>
2025-07-25 08:37:15 +0200

    quick commit

--- gtfs_railways/tests/conftest.py
+++ gtfs_railways/tests/conftest.py
@@ -1,42 +1,29 @@
 import pytest # type: ignore
 
+from gtfs_railways.utils.imports import *
+
+"""
 from gtfs_railways.utils.config import DATA_DIR
 from gtfs_railways.functions.core import load_gtfs
 from gtfs_railways.functions.core import load_graph
+"""
 
 from gtfs_railways.functions.v0 import get_all_GTC as get_all_GTC_0
 from gtfs_railways.functions.v1 import P_space as P_space_1, get_all_GTC as get_all_GTC_1
 
-
 @pytest.fixture
 def L_space_10_path():
     # Use the graph_0.pkl file from the examples directory
-    print(DATA_DIR)
-    print(DATA_DIR)
-    print(DATA_DIR)
-    print(DATA_DIR)
-    print(DATA_DIR)
-    print(DATA_DIR)
     return str( DATA_DIR / "pkl/10/graph_0.pkl" )
 
 @pytest.fixture
 def L_space_20_path():
     # Use the graph_0.pkl file from the examples directory
-    print(DATA_DIR)
-    print(DATA_DIR)
-    print(DATA_DIR)
-    print(DATA_DIR)
-
     return str( DATA_DIR / "pkl/20/graph_0.pkl" )
 
 @pytest.fixture
 def L_space_many():
     # Use the graph_0.pkl file from the examples directory
-    print(DATA_DIR)
-    print(DATA_DIR)
-    print(DATA_DIR)
-    print(DATA_DIR)
-
     return str( DATA_DIR / "pkl/subgraphs_by_size.pkl" )
 
 @pytest.fixture
